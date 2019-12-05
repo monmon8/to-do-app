e32bf1b85428ae98c55e721b8a11eb439ec6da40
Ahmed <imaneben8@gmail.com>
2019-12-05 19:15:42 +0000

    Creating the User Interface

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,29 +1,16 @@
-import React from 'react';
-import MyComponent from './MyComponent';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h2>Welcome to React</h2>
-        <MyComponent />
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+import TodoList from './TodoList'
+
+import './App.css'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoList />
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
